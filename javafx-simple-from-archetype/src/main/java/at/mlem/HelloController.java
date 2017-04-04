4a56376d7889fbd9fb909200344f8730d56904b6
Martin Lemanski <martin.lemanski@gmx.at>
2017-04-05 01:11:09 +0200

    add paste functionality with javafx when you paste one word, it will be put into the first name, if more, the first word will be first name, all other words will be last name

--- javafx-simple-from-archetype/src/main/java/at/mlem/HelloController.java
+++ javafx-simple-from-archetype/src/main/java/at/mlem/HelloController.java
@@ -6,21 +6,31 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.input.*;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-public class HelloController  implements Initializable
-{
+import static java.util.stream.Collectors.joining;
+
+public class HelloController implements Initializable {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
-
-    @FXML private Menu tiereMenu;
-    @FXML private TextField firstNameField;
-    @FXML private TextField lastNameField;
-    @FXML private Label messageLabel;
+    private static final Pattern BLANK_PATTERN = Pattern.compile(" ");
+    private static final KeyCombination PASTE_COMBINATION = new KeyCodeCombination(KeyCode.V,
+            KeyCombination.CONTROL_DOWN);
+    @FXML
+    private Menu tiereMenu;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private Label messageLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -60,4 +70,20 @@ public class HelloController  implements Initializable
         }
     }
 
+    public void paste(KeyEvent keyEvent) {
+        if (PASTE_COMBINATION.match(keyEvent) && !(keyEvent.getTarget() instanceof TextField)) {
+            log.debug("entering paste operation on {} on target {} ({})", PASTE_COMBINATION, keyEvent.getTarget(), keyEvent);
+            final Clipboard clipboard = Clipboard.getSystemClipboard();
+            if (clipboard.hasString()) {
+                String[] clipboardTokens = clipboard.getString().split(BLANK_PATTERN.pattern());
+                if (clipboardTokens.length >= 1) {
+                    firstNameField.setText(clipboardTokens[0]);
+                    if (clipboardTokens.length >= 2) {
+                        lastNameField.setText(Stream.of(clipboardTokens).skip(1).collect(joining(" ")));
+                    }
+                }
+            }
+            keyEvent.consume();
+        }
+    }
 }
